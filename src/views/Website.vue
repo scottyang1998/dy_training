6b4efa29402c4fe7e82cb0283bc4ba106dd4374d
ydc <395670149@qq.com>
2020-12-22 21:03:03 +0800

    website pic prepare

--- src/views/Website.vue
+++ src/views/Website.vue
@@ -1,5 +1,53 @@
 <template>
   <div class="website">
-    <h1>This is an official website</h1>
+    <div class="s1">
+      <h1 class="hide">多益网络官网</h1>
+      <div class="head">
+        <div class="bd">
+          <div class="logo"></div>
+          <div class="nav"></div>
+        </div>
+      </div>
+      <div class="focus"></div>
+      <div class="recommend"></div>
+      <div class="gamelist"></div>
+    </div>
+    <div class="foot">
+
+    </div>
   </div>
 </template>
+
+<style scoped>
+  body,
+  div,
+  ul,
+  ol,
+  dl,
+  dd,
+  dt,
+  li,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  .hide {
+    display: none;
+  }
+
+  .head {
+    height: 65px;
+    background-color: #262a2d;
+  }
+
+  .head .bd {
+    width: 1000px;
+  }
+</style>
